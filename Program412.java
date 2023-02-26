6151293fb1bc299d9181dbccdbe01c7d19666855
Nagaraju642 <126244560+Nagaraju642@users.noreply.github.com>
2023-02-26 17:14:59 +0530

    Add files via upload

---
+++ Program412.java
@@ -0,0 +1,13 @@
+public class Program412
+{
+public static void main(String[] args)
+{
+//creat an array
+int[]numbers={4,7,5,-5};
+//iterating through the array
+for(int number:numbers)
+{
+System.out.println("number");
+}
+}
+}
---
+++ Program415.java
@@ -0,0 +1,11 @@
+public class Program415
+{
+public static void main(String[] args)
+{
+int sum=0;
+for(byte i=1;i<=10;--i)
+{
+System.out.println("Raju"+i);
+}
+}
+}
