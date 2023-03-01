3b8bfeead034a905ee0f84718c364226215ad178
Nagaraju642 <126244560+Nagaraju642@users.noreply.github.com>
2023-03-01 15:47:21 +0530

    Add files via upload

---
+++ DayPrint.java
@@ -0,0 +1,25 @@
+class MyDate
+{
+private int day;
+public int month;
+public int year;
+public int get_day()
+{
+return day;
+}
+public void set_day(int d)
+{
+day=d;
+}
+}
+class DayPrint
+{
+public static void main(String[] args)
+{
+MyDate ob1=new MyDate();
+ob1.set_day(14);
+ob1.month=8;
+ob1.year=2011;
+System.out.println(ob1.get_day()+"/"+ob1.month+"/"+ob1.year);
+}
+}
