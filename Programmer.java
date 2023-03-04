87be884218f4f468bd08196f2c851661ee8874d0
Nagaraju642 <126244560+Nagaraju642@users.noreply.github.com>
2023-03-04 09:39:16 +0530

    Add files via upload

---
+++ Programmer.java
@@ -0,0 +1,14 @@
+class Empolyee
+{
+float salary=40000;
+}
+class Programmer extends Empolyee
+{
+int bonus=10000;
+public static void main(String[] args)
+{
+Programmer P=new Programmer();
+System.out.println("Programmer salary is:"+P.salary);
+System.out.println("Bonus of Programmer is:"+P.bonus);
+}
+}
